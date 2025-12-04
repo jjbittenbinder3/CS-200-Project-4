a3a0c69259551c364374af9ee6f3dfd324aa3bd9
TheSirJ <jepepper@crimson.ua.edu>
2025-12-04 15:33:35 -0600

    Update Member.java

--- src/model/Member.java
+++ src/model/Member.java
@@ -1,1 +1,20 @@
+package model;
 
+public class Member {
+  private String name;
+  private String number;
+  private String address;
+  private String city;
+  private String state;
+  private String zip;
+  private boolean suspended;
+  
+  public Member() {}
+  
+  public Member(String name, String number) {
+    this.name = name;
+    this.number = number;
+}
+
+  // Getters and setters
+}
