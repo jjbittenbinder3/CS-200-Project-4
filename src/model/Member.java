b36d8a2a51af22baed7132f243ef1f8c9c3f3517
TheSirJ <jepepper@crimson.ua.edu>
2025-12-04 15:54:08 -0600

    Update Member.java

--- src/model/Member.java
+++ src/model/Member.java
@@ -2,28 +2,47 @@ package model;
 
 public class Member {
   private String name;
+  private int ID;
+  private String password;
+  
   private String address;
   private String city;
   private String state;
   private String zip;
+
   private boolean suspended;
-  private int ID;
-  private String password;
-  
+
+  //Constructors
   public Member() {}
   
-  public Member(String name, int ID) {
+  public Member(String name, int ID, String password = "", boolean suspended = false) {
     this.name = name;
-    this.number = number;
+    this.ID = ID;
+    this.password = password;
+    this.suspended = suspended;
   }
 
+  //Public Methods
   public Login(String inID, String inPassword) {
-    if (inID == this.ID) && (inPassword == this.password) {
-      return true;
-    }
+    if (inID == this.ID) && (inPassword == this.password) return true;
     return false;
   }
-  // Getters and setters
-}
 
-newMember = new Member(name, ID);
+  public SetInfo(String name, int ID, string password, boolean suspended = this.suspended) {
+    this.name = name;
+    this.ID = ID;
+    this.password = password;
+    this.suspended = suspended;
+  }
+
+  public SetAddress(String address, String city, String state, string zip) {
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+  }
+
+  public SetStatus(boolean suspended) {
+    this.suspended = suspended;
+  }
+}
