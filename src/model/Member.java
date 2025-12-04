4b47cf7d01b8e7fda237fecad07d0af8cd3d5490
TheSirJ <jepepper@crimson.ua.edu>
2025-12-04 15:38:11 -0600

    Update Member.java

--- src/model/Member.java
+++ src/model/Member.java
@@ -2,19 +2,28 @@ package model;
 
 public class Member {
   private String name;
-  private String number;
   private String address;
   private String city;
   private String state;
   private String zip;
   private boolean suspended;
+  private int ID;
+  private String password;
   
   public Member() {}
   
-  public Member(String name, String number) {
+  public Member(String name, int ID) {
     this.name = name;
     this.number = number;
-}
+  }
 
+  public Login(String inID, String inPassword) {
+    if (inID == this.ID) && (inPassword == this.password) {
+      return true;
+    }
+    return false;
+  }
   // Getters and setters
 }
+
+newMember = new Member(name, ID);
