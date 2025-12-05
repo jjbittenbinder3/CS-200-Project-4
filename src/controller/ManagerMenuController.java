b89121b50ae6fc3ae8415c2fe59ec3413f3b876a
TheSirJ <jepepper@crimson.ua.edu>
2025-12-05 02:59:15 -0600

    Update ManagerMenuController.java

--- src/controller/ManagerMenuController.java
+++ src/controller/ManagerMenuController.java
@@ -6,8 +6,7 @@ package controller;
 
 public class ManagerMenuController {
   public ManagerMenuController()  {
-    LoginController loginMenu = new LoginController();
-    boolean valid = loginMenu.login(LoginController.type.MANAGER);
+    boolean valid = new LoginController().login(LoginController.Type.MANAGER);
     if (!valid) return;
     System.out.println("");
     // here
