4578d8d856ded1ba7662b5177beb4954d6f8e392
TheSirJ <jepepper@crimson.ua.edu>
2025-12-04 20:57:55 -0600

    Create Service.java

---
+++ src/model/Service.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Service {
+
+  //Variables
+  
+  private String name;
+  private int code;
+  private double fee;
+
+  //Constructors
+  
+  public Service() {}
+  
+  public Service(String name, String code, double fee) {
+    this.name = name;
+    this.code = code;
+    this.fee = fee;
+  }
+  
+  // Getters and setters
+
+  
+}
