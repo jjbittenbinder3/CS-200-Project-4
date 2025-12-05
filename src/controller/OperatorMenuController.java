8fdc1e46edca90adf1b6f07a007e94ebc262ecc0
TheSirJ <jepepper@crimson.ua.edu>
2025-12-05 02:59:40 -0600

    Update OperatorMenuController.java

--- src/controller/OperatorMenuController.java
+++ src/controller/OperatorMenuController.java
@@ -6,8 +6,7 @@ package controller;
 
 public class OperatorMenuController {
   public OperatorMenuController() {
-    LoginController loginMenu = new LoginController();
-    boolean valid = loginMenu.login(LoginController.type.OPERATOR);
+    boolean valid = new LoginController().login(LoginController.type.OPERATOR);
     if (!valid) return;
     System.out.println("");
     // here
