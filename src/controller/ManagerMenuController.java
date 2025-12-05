418353bac6c61ff1d1a33338c857b00587d331fb
TheSirJ <jepepper@crimson.ua.edu>
2025-12-04 22:06:17 -0600

    Update ManagerMenuController.java

--- src/controller/ManagerMenuController.java
+++ src/controller/ManagerMenuController.java
@@ -1,1 +1,14 @@
+/*
+  Author: James Pepper
+*/
 
+package controller;
+
+public class ManagerMenuController {
+  public ManagerMenuController();
+  
+  public void showManagerMenu() {
+    System.out.println("");
+    // here
+  }
+}
