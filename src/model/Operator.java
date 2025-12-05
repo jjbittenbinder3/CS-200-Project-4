755048c113edb6a1def48df15e12e0925af6e5ad
TheSirJ <jepepper@crimson.ua.edu>
2025-12-05 15:04:05 -0600

    Create Operator.java

---
+++ src/model/Operator.java
@@ -0,0 +1,74 @@
+/*
+  Author: James Zittlow
+*/
+
+package model;
+
+public class Operator {
+
+  //Variables
+  
+  private String name;
+  private int ID;
+  private String password;
+  
+  private String phoneNumber;
+  private String email;
+
+  //Constructors
+  
+  public Operator() {}
+  
+  public Operator(String name, int ID) {
+    this.name = name;
+    this.ID = ID;
+  }
+  
+  public Operator(String name, int ID, String password) {
+    this.name = name;
+    this.ID = ID;
+    this.password = password;
+  }
+
+  //Public Methods
+  
+  public boolean login(int ID, String Password) {
+    if ((ID == this.ID) && (Password.equals(this.password))) return true;
+    return false;
+  }
+
+  //Setters
+
+  public void setInfo(String name, int ID, String password) {
+    this.name = name;
+    this.ID = ID;
+    this.password = password;
+  }
+
+  public void setContactInfo(String phoneNumber, String email) {
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  }
+
+  //Getters
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int getID() {
+    return this.ID;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public String getPhoneNumber() {
+    return this.phoneNumber;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+}
