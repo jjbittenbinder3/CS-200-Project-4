eda95dd478d7a8bda0c69feaa0b6baf82ee4cb5f
TheSirJ <jepepper@crimson.ua.edu>
2025-12-05 03:08:02 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ import controller.MainMenuController;
 public class Main {
   public Main() {
     new MainMenuController();
+    System.out.println("terminal exiting...");
   }
-  System.out.println("terminal exiting...")
 }
