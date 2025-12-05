10fe272b3a954c7738be91eedb1e336bddeee759
TheSirJ <jepepper@crimson.ua.edu>
2025-12-05 02:08:29 -0600

    Update MemberDatabase.java

--- src/data/MemberDatabase.java
+++ src/data/MemberDatabase.java
@@ -1,1 +1,39 @@
+/*
+  Author: James Pepper
+*/
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+package data;
+
+public class MemberDatabase() {
+
+  public MemberDatabase() {
+    try(BufferedReader read = new BufferedReader(new FileReader(csvFile))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        String[] fields = line.split(",");
+        for (String f : fields)
+          System.out.println(f);
+        }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
+
+
+
+public class CSVReaderSimple {
+    public static void main(String[] args) {
+        String csvFile = "data/file.csv";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            
+
+        
+            
+        }
+    }
