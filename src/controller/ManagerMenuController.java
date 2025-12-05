4ce57f75713e75e843b69580de6632faae0118c0
jzittlow <jzittlowcollege@gmail.com>
2025-12-05 15:10:17 -0600

    Added Manager Menu Controller Logic

--- src/controller/ManagerMenuController.java
+++ src/controller/ManagerMenuController.java
@@ -1,5 +1,5 @@
 /*
-  Author: James Pepper
+  Author: James Pepper & James Zittlow
 */
 
 package controller;
@@ -14,15 +14,42 @@ public class ManagerMenuController {
     int status = new LoginController().login(LoginController.Type.MANAGER);
     switch (status) {
       case 0:
-        System.out.println("Invalid member ID entered.");
+        System.out.println("Invalid manager ID entered.");
         return;
       case 1:
         System.out.println("Invalid password entered.");
         return;
       case 2:
-        //here
+        int input = 0;
+        while (input != 4) {
+          System.out.println("1. Member Report");
+          System.out.println("2. Provider Report");
+          System.out.println("3. Summary Report");
+          System.out.println("4. Exit");
+          Scanner scanner = new Scanner(System.in);
+          input = scanner.nextInt();
+          switch (input) {
+            case 1:
+              new MemberReport();
+              System.out.println("Enter Member ID: ");
+              int id = scanner.nextInt();
+              MemberReport.generateMemberReport(id);
+              break;
+            case 2:
+              new ProviderReport();
+              System.out.println("Enter Provider ID: ");
+              int id = scanner.nextInt();
+              ProviderReport.generateProviderReport(id);
+              break;
+            case 3:
+              new SummaryReport();
+              SummaryReport.generateSummaryReport();
+              break;
+            case 4:
+              return;
+          }
+        }
         return;
     }
-    return;
   }
 }
