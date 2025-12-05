7b7f975eadf3e1975a4c9dd6618228b5aec24808
TheSirJ <jepepper@crimson.ua.edu>
2025-12-04 22:05:46 -0600

    Update OperatorMenuController.java

--- src/controller/OperatorMenuController.java
+++ src/controller/OperatorMenuController.java
@@ -1,1 +1,14 @@
+/*
+  Author: James Pepper
+*/
 
+package controller;
+
+public class OperatorMenuController {
+  public OperatorMenuController();
+  
+  public void showOperatorMenu() {
+    System.out.println("");
+    // here
+  }
+}
