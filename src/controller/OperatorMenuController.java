24fd973e0445980f4aec211477a77cf84fb6806c
TheSirJ <jepepper@crimson.ua.edu>
2025-12-05 14:15:02 -0600

    Update OperatorMenuController.java

--- src/controller/OperatorMenuController.java
+++ src/controller/OperatorMenuController.java
@@ -6,9 +6,22 @@ package controller;
 
 public class OperatorMenuController {
   public OperatorMenuController() {
-    boolean valid = new LoginController().login(LoginController.Type.OPERATOR);
-    if (!valid) return;
-    System.out.println("");
-    // here
+    showMenu();
+  }
+  
+  private showMenu() {
+    int status = new LoginController().login(LoginController.Type.OPERATOR);
+    switch (status) {
+      case 0:
+        System.out.println("Invalid operator ID entered.");
+        return;
+      case 1:
+        System.out.println("Invalid password entered.");
+        return;
+      case 2:
+        //here
+        return;
+    }
+    return;
   }
 }
