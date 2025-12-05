cbb74ff5763f379b517e0723b24baab2a9477237
TheSirJ <jepepper@crimson.ua.edu>
2025-12-05 14:11:44 -0600

    Update ManagerMenuController.java

--- src/controller/ManagerMenuController.java
+++ src/controller/ManagerMenuController.java
@@ -5,10 +5,23 @@
 package controller;
 
 public class ManagerMenuController {
+  
   public ManagerMenuController()  {
-    boolean valid = new LoginController().login(LoginController.Type.MANAGER);
-    if (!valid) return;
-    System.out.println("");
-    // here
+    showMenu();
+  }
+  
+  private showMenu() {
+    int status = new LoginController().login(LoginController.Type.MANAGER);
+    switch (status) {
+      case 0:
+        System.out.println("Invalid member ID entered.");
+        return;
+      case 1:
+        System.out.println("Invalid password entered.");
+        return;
+      case 2:
+        
+    }
+    return;
   }
 }
