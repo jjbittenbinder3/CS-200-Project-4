00ac07296f3b099fc38984abdc67ab4cec1a037d
TheSirJ <jepepper@crimson.ua.edu>
2025-12-05 02:53:30 -0600

    Update and rename system.java to Main.java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+  Author: James Pepper
+*/
+
+import controller.MainMenuController;
+
+public class Main() {
+  public Main() {
+    new MainMenuController();
+  }
+  System.out.println("terminal exiting...")
+}
