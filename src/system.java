6e4ba1b9ae980afff1891fdeda7355d36fafe840
TheSirJ <jepepper@crimson.ua.edu>
2025-12-04 22:18:56 -0600

    Update system.java

--- src/system.java
+++ src/system.java
@@ -1,1 +1,13 @@
+/*
+  Author: James Pepper
+*/
 
+import controller.MainMenuController;
+
+public class System() {
+  public System() {
+    MainMenuController menu = new MainMenuController();
+    menu.showMainMenu();
+  }
+  System.out.println("terminal exiting...")
+}
