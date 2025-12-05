0f70c514363cf3e56597a88a5131c232fefbfaa5
TheSirJ <jepepper@crimson.ua.edu>
2025-12-05 15:09:49 -0600

    Update LoginController.java

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -27,52 +27,51 @@ public class LoginController {
   public int login(Type loginType) {
     Scanner scan = new Scanner(System.in);
     int inID = 0;
-    int inPass = 0;
+    String inPass = "";
     
     switch (loginType) {
       case MEMBER:
         System.out.print("Enter Member ID: ");
-        inID = scan.nextInt()
-        Member mem = new MemberService().findMember(inID);
-        if (!mem) return 0;
+        inID = scan.nextInt();
+        Member mem = new MemberService().getMember(inID);
+        if (mem == null) return 0;
           System.out.print("Enter password: ");
-          inPass = scan.nextInt();
-        if (mem.password != inPass) return 1;
+          inPass = scan.next();
+        if (mem.getPassword() != inPass) return 1;
         return 2;
         
       case OPERATOR:
         System.out.print("Enter Operator ID: ");
-        inID = scan.nextInt()
-        Operator op = new OperatorService().findOperator(inID);
-        if (!op) return 0;
+        inID = scan.nextInt();
+        Operator op = new OperatorService().getOperator(inID);
+        if (op == null) return 0;
           System.out.print("Enter password: ");
-          inPass = scan.nextInt();
-        if (op.password != inPass) return 1;
+          inPass = scan.next();
+        if (op.getPassword() != inPass) return 1;
         return 2;
         
       case PROVIDER:
         System.out.print("Enter Provider ID: ");
-        inID = scan.nextInt()
-        Provider prov = new ProviderService().findProvider(inID);
-        if (!prov) return 0;
+        inID = scan.nextInt();
+        Provider prov = new ProviderService().getProvider(inID);
+        if (prov == null) return 0;
           System.out.print("Enter password: ");
-          inPass = scan.nextInt();
-        if (prov.password != inPass) return 1;
+          inPass = scan.next();
+        if (prov.getPassword() != inPass) return 1;
         return 2;
         
       case MANAGER:
         System.out.print("Enter Manager ID: ");
-        inID = scan.nextInt()
-        Manager man = new ManagerService().findManager(inID);
-        if (!man) return 0;
+        inID = scan.nextInt();
+        Manager man = new ManagerService().getManager(inID);
+        if (man == null) return 0;
           System.out.print("Enter password: ");
-          inPass = scan.nextInt();
-        if (man.password != inPass) return 1;
+          inPass = scan.next();
+        if (man.getPassword() != inPass) return 1;
         return 2;
         
       default:
         return 0;
     }
-    return 0;
   }
 }
