330053b82748da15ce612ea5eca13a20d7a6a506
TheSirJ <jepepper@crimson.ua.edu>
2025-12-04 22:32:47 -0600

    Update LoginController.java

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -3,7 +3,26 @@
 */
 
 public class LoginController() {
-  public LoginController() {
-    //here
+  
+  enum type {
+    MEMBER,
+    OPERATOR,
+    PROVIDER,
+    MANAGER
+  }
+  
+  public LoginController(type loginType) {
+    switch (loginType) {
+      case MEMBER:
+        break;
+      case OPERATOR:
+        break;
+      case PROVIDER;
+        break;
+      case MANAGER:
+        break;
+      default:
+        break;
+    } 
   }
 }
