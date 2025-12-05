f5e578a7ed77995cbe774a3c66f522c8272b6798
TheSirJ <jepepper@crimson.ua.edu>
2025-12-04 22:33:58 -0600

    Update ProviderMenuController.java

--- src/controller/ProviderMenuController.java
+++ src/controller/ProviderMenuController.java
@@ -6,6 +6,9 @@ package controller;
 
 public class ProviderMenuController {
   public ProviderMenuController() {
+    LoginController loginMenu = new LoginController();
+    boolean valid = loginMenu.login(LoginController.type.PROVIDER);
+    if (!valid) return;
     System.out.println("");
     // here
   }
